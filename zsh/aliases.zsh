31e70665d05bb60446577592b60a3d1455e1a89e
Paul Matthews <pmatthews@google.com>
2014-08-20 14:01:03 -0700

    Adding ZSH changes
    * Merging previous zshrc config into dotfiles structure

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,35 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias mv='nocorrect mv'       # no spelling correction on mv
+alias cp='nocorrect cp'       # no spelling correction on cp
+alias mkdir='nocorrect mkdir' # no spelling correction on mkdir
+alias pu=pushd
+alias po=popd
+alias d='dirs -v'
+alias h=history
+alias grep=egrep
+alias ll='ls -l'
+alias la='ls -a'
+
+# List only directories and symbolic
+# links that point to directories
+alias lsd='ls -ld *(-/DN)'
+
+# List only file beginning with "."
+alias lsa='ls -ld .*'
+
+alias gvimdiff="mvimdiff"
+alias vim="vim -p"
+
+# Global aliases -- These do not have to be
+# at the beginning of the command line.
+alias -g M='| more'
+alias -g H='| head'
+alias -g T='| tail'
+alias -g L='| less'
+alias -g G="| grep"
+alias -g L="| less"
+
+# copy with a progress bar.
+alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,6 +6,8 @@ fi
 
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
+export EDITOR="vim"
+export PAGER=most
 
 fpath=($ZSH/functions $fpath)
 
@@ -39,10 +41,127 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+bindkey -v               # vi key bindings
+bindkey ' ' magic-space    # also do history expansion on space
+bindkey '^R' history-beginning-search-backward
+bindkey '^S' history-beginning-search-backward
+bindkey -M viins '\C-i' complete-word
+
+# Faster! (?)
+zstyle ':completion::complete:*' use-cache 1
+
+#{{{ Options
+
+# why would you type 'cd dir' if you could just type 'dir'?
+setopt AUTO_CD
+
+# Now we can pipe to multiple outputs!
+setopt MULTIOS
+
+# Spell check commands!  (Sometimes annoying)
+setopt CORRECT
+
+# This makes cd=pushd
+setopt AUTO_PUSHD
+
+# This will use named dirs when possible
+setopt AUTO_NAME_DIRS
+
+# If we have a glob this will expand it
+setopt GLOB_COMPLETE
+setopt PUSHD_MINUS
+
+# blank pushd goes to home
+setopt PUSHD_TO_HOME
+
+# this will ignore multiple directories for the stack.  Useful?  I dunno.
+setopt PUSHD_IGNORE_DUPS
+
+# use magic (this is default, but it can't hurt!)
+setopt ZLE
+setopt NO_HUP
+setopt VI
+setopt IGNORE_EOF
+
+# If I could disable Ctrl-s completely I would!
+setopt NO_FLOW_CONTROL
+
+# beeps are annoying
+setopt NO_BEEP
+
+# Keep echo "station" > station from clobbering station
+setopt NO_CLOBBER
+
+# Case insensitive globbing
+setopt NO_CASE_GLOB
+
+# Be Reasonable!
+setopt NUMERIC_GLOB_SORT
+
+# I don't know why I never set this before.
+setopt EXTENDED_GLOB
+
+# hows about arrays be awesome?  (that is, frew${cool}frew has frew surrounding all the variables, not just first and last
+setopt RC_EXPAND_PARAM
+
+#}}}
+
+#{{{ History Stuff
+
+# Where it gets saved
+HISTFILE=~/.history
+
+# Remember about a years worth of history (AWESOME)
+SAVEHIST=10000
+HISTSIZE=10000
+
+# Don't overwrite, append!
+setopt APPEND_HISTORY
+
+# Killer: share history between multiple shells
+setopt SHARE_HISTORY
+
+# If I type cd and then cd again, only save the last one
+setopt HIST_IGNORE_DUPS
+
+# Pretty    Obvious.  Right?
+setopt HIST_REDUCE_BLANKS
+
+# If a line starts with a space, don't save it.
+setopt HIST_IGNORE_SPACE
+setopt HIST_NO_STORE
+
+# When using a hist thing, make a newline show the change before executing it.
+setopt HIST_VERIFY
+
+# Save the time and how long a command ran
+setopt EXTENDED_HISTORY
+
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_FIND_NO_DUPS
+
+#}}}
+
+#{{{ Functions
+
+_force_rehash() {
+    (( CURRENT == 1 )) && rehash
+    return 1  # Because we didn't really complete anything
+}
+
+edit-command-output() {
+    BUFFER=$(eval $BUFFER)
+    CURSOR=0
+}
+zle -N edit-command-output
+
+#}}}
+
+
+#{{{ ZSH Modules
+
+autoload -U compinit zcalc zsh-mime-setup
+zsh-mime-setup
+
+#}}}
