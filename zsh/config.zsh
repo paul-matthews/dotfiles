c364778f845a362714e339f31d490779a3be69af
Paul Matthews <pmatthews@google.com>
2014-08-27 10:08:59 -0700

    Formatting files
    * Formatting config.zsh
    * Also removing hard to see colors from the prompt

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,7 +4,7 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
+# export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 export EDITOR="vim"
 export PAGER=most
