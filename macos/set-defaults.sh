e4d5ccf8f710140b76c78867f4e6bef723da02a5
Paul Matthews <pmatthews@google.com>
2015-07-06 15:33:38 -0700

    Setting some defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -25,6 +25,7 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 
 # Run the screensaver if we're in the bottom-left hot corner.
 defaults write com.apple.dock wvous-bl-corner -int 5
